a6ed42e0abbb56492595ac57fb315ef34c387185
Philipp Schroeppel <69214052+pns1123@users.noreply.github.com>
2023-10-29 12:15:35 +0100

    remove directories from git tracking

--- session01/printf.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	printf("Das ist die Ausgabe der Ganzzahl %d,\n"
-	       "der Gleitkommezahl %f und des Strings %s\n",
-	       42, 3.1415926, "Hello, World!");
-}
